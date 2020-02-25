e760ad09d4693bff6e00250be5e48b3fddb98060
Andriy <pvm150703@gmail.com>
2020-02-25 23:35:51 +0200

    Fixed bug with position and view of cart and username in header on small windows

--- javascript/components.js
+++ javascript/components.js
@@ -1,13 +1,16 @@
 let Vheader = {
   props: ['items-length'],
   template: `<header class="navbar navbar-expand-md navbar-dark bg-primary text-light">
-			<a href="#" class="navbar-brand mr-5">
+			<div class="navbar-header">
+				<a href="#" class="navbar-brand mr-5">
 				<strong class="text-warning">BestShop</strong>
-			</a>
-			<button class="navbar-toggler" data-toggle="collapse" data-target="#md">
-			<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="navbar-collapse collapse" id="md">
+				</a>
+				<button class="navbar-toggler" data-toggle="collapse" data-target="#header">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+			</div>
+
+			<div class="navbar-collapse collapse" id="header">
 				<ul class="navbar-nav bd-navbar-nav">
 					<li class="nav-item">
 						<a class="nav-link text-warning rounded-pill" href="#" >
@@ -15,7 +18,7 @@ let Vheader = {
 						</a>
 					</li>
 					<li class="navbar-item">
-						<a href="shop.html" class="nav-link active text-warning bg-info rounded-pill border border-info">
+						<a href="#" class="nav-link active text-warning bg-info rounded-pill border border-info">
 							<b>Shop</b>
 						</a>
 					</li>
@@ -26,29 +29,30 @@ let Vheader = {
 					</li>
 				</ul>
 			</div>
-			<div class="navbar-collapse collapse float-right">
-				<ul class="navbar-nav nav-flex-icons">
-					<li class="nav-item">
-						<a href="#" class="nav-link ">
-							<span class="badge bg-danger text-white z-depth-1 mr-1">
-								{{items-length}}
-							</span>
-							<i class="fa fa-shopping-cart"></i>
-							<span class="d-none d-sm-inline-block text-warning">
-								<strong>Cart</strong>
-							</span>
-						</a>
-					</li>
-					<li class="nav-item">
-						<a href="#" class="nav-link">
-							<i class="fa fa-user"></i>
-							<span class="d-none d-sm-inline-block text-warning">
-							<strong>User.name</strong>
+			<ul class="nav navbar-nav nav-flex-icons">
+				<li class="nav-item">
+					<a href="#" class="nav-link ">
+						<i class="fa fa-shopping-cart">
+							<sup>
+								<span class="badge bg-danger text-white z-depth-1 mr-1">
+									{{items-length}}
+								</span>
+							</sup>
+						</i>
+						<span class="d-sm-inline-block text-warning">
+							<strong>Cart</strong>
 						</span>
-						</a>
-					</li>
-				</ul>
-			</div>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link">
+						<i class="fa fa-user"></i>
+						<span class="d-sm-inline-block text-warning">
+						<strong>User.name</strong>
+					</span>
+					</a>
+				</li>
+			</ul>
 		</header>`,
   methods: {
 
